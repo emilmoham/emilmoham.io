6a4ff35bcaa74b394fd434c4375e06f5ddada89e
Emil Mohammed <emilmoham@gmail.com>
2022-06-28 16:11:26 -0400

    initial styling for blog page

--- personal-site-v4/pages/blog/[uuid].js
+++ personal-site-v4/pages/blog/[uuid].js
@@ -1,14 +1,28 @@
 import Head from 'next/head';
+import styles from '../../styles/blog.module.css'
+
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import React from 'react';
 
 function Blog({ post }) {
     return (
         <div>
-            <div>{post.uuid}</div>
-            <div>{post.title}</div>
-            <div>{post.created}</div>
-            <div>{post.updated}</div>
-            <div>{post.short_description}</div>
-            <div>{post.content}</div>
+            <Head>
+                <title>{post.title}</title>
+                <meta name="description" content={post.short_description} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.item + styles.item_main}>
+                    <h1>{post.title}</h1>
+                    <p className={styles.blogDate}>{new Date(post.created).toDateString()}</p>
+                    <ReactMarkdown>{post.content}</ReactMarkdown>
+                </div>
+                <div className={styles.item + styles.item_aside}>
+                    <h4 id="blogTagsHeader">Tags:</h4>
+                    <p><b>Coming soon:</b> a tag system to search for related blog posts!</p>
+                </div>
+            </div>
         </div>
     );
 }
