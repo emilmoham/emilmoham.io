8ea59ed927bf6dc2908b2c04827a2b6fdce9b27e
Emil Mohammed <emilmoham@gmail.com>
2022-06-30 17:42:17 -0400

    starting to work on archive

---
+++ personal-site-v4/components/ArchiveListing.js
@@ -0,0 +1,11 @@
+import blogDefault from '../public/blog_default.png'
+
+export default function ArchiveListing({ props }) {
+    return (
+        <div >
+            <div>
+                <img src={blogDefault} />
+            </div>
+        </div>
+    );
+}
--- personal-site-v4/pages/archive.js
+++ personal-site-v4/pages/archive.js
@@ -1,3 +1,24 @@
-export default function Archive () {
-  return <>TODO</>
+import ArchiveListing from "../components/ArchiveListing";
+
+function Archive ({ posts }) {
+  return (
+    <div>
+      {
+        posts.map(function(blog, index){
+          return (
+            <ArchiveListing props={index} />
+          );
+        })
+      }
+    </div>
+  );
 }
+
+export async function getStaticProps(){
+  const res = await fetch('https://api.emilmoham.io/blog/list');
+  const posts = await res.json();
+
+  return { props: { posts } }
+}
+
+export default Archive;
